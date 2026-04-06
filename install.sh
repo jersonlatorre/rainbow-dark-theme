1de0adc6242eceac0640125ad3edc07d4ad99603
Jerson La Torre <jerson.latorre@gmail.com>
2026-04-06 14:34:04 -0500

    chore: update install script to use cursor and replace vsix package name

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 jq 'del(.tokenColors[] | select(.name == "js/ts italic"))' ./themes/vibrant-dark-color-theme.json > tmp.json && mv tmp.json ./themes/vibrant-dark-color-theme.json
 vsce package
-code --install-extension vibrant-dark-color-theme-0.0.1.vsix
+cursor --install-extension vibrant-dark-color-theme-0.0.1.vsix
 git add .
 git commit -m "update"
 git push
